8b539fbc95926f1ced6c763251ff85c55b232988
Jacky Chu <jacky.chu@nutanix.com>
2021-10-28 13:15:40 -0700

    add smoke tests

--- src/test/java/com/nutanix/simpleservice/SimpleServiceApplicationTests.java
+++ src/test/java/com/nutanix/simpleservice/SimpleServiceApplicationTests.java
@@ -1,10 +1,20 @@
 package com.nutanix.simpleservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SimpleServiceApplicationTests {
+
+	@Autowired
+	GreetingController greetingController;
+
+	@Autowired
+	HealthCheckController healthCheckController;
+
+	@Autowired
+	VersionController versionController;
 
 	@Test
 	void contextLoads() {
